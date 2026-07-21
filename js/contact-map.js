3ee31da28f41b1bc46d6cc02e5e9a08087801f16
Marijus Ravickas <989645+marijus-ravickas@users.noreply.github.com>
2026-07-21 22:49:54 +0300

    Switch contact map to CARTO Voyager tiles

--- js/contact-map.js
+++ js/contact-map.js
@@ -4,9 +4,10 @@ document.addEventListener('DOMContentLoaded', function () {
   var lat = parseFloat(el.dataset.lat);
   var lng = parseFloat(el.dataset.lng);
   var map = L.map('contact-map', { scrollWheelZoom: false }).setView([lat, lng], 16);
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 19
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    maxZoom: 20,
+    subdomains: 'abcd'
   }).addTo(map);
   L.marker([lat, lng]).addTo(map).bindPopup(el.dataset.address);
 });
