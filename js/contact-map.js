ac8c7b47fb45d3a4ee46628b50e03f4830ad9b8b
Marijus Ravickas <989645+marijus-ravickas@users.noreply.github.com>
2026-07-22 00:17:14 +0300

    Deploy: SEO/AEO improvements + Lithuanian (LT) site

--- js/contact-map.js
+++ js/contact-map.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var lat = parseFloat(el.dataset.lat);
   var lng = parseFloat(el.dataset.lng);
   var map = L.map('contact-map', { scrollWheelZoom: false }).setView([lat, lng], 16);
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     maxZoom: 20,
     subdomains: 'abcd'
